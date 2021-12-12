5a55956384cb5c233ea7f47ff5dff6351e7fbd5e
NHANSMALL999 <20120341@student.hcmus.edu.vn>
2021-12-13 00:41:09 +0700

    Hoàn thành đăng nhập/đăng ký Chưa ghép threading.

--- server.py
+++ server.py
@@ -8,82 +8,85 @@ FORMAT="utf_16"
 def RecieveList(conn):
     list = []
     data = None
+    data= conn.recv(1024).decode(FORMAT)
     while(data!="end"):
         
         list.append(data)
         conn.send(data.encode(FORMAT))
         data= conn.recv(1024).decode(FORMAT)
     return list
-    print("recieve:")
-    print(list)
 
-#Hàm kiểm tra id và pw
-def AccountCheck(id,pw):
-    conx = pyodbc.connect(
-    "Driver={ODBC Driver 17 for SQL Server};"
-    "Server=DESKTOP-S8G0HJG\SQLEXPRESS;"
-    "Database=ACCOUNT;"
-    "Trusted_Connection=yes;")
+#Hàm kiểm tra id và pw      
+def AccountCheck(conn):
     temp = ""
-    account = RecieveList(conn)
-    cursor = conx.cursor()
-    for row in cursor.execute("select PASSWORD from INFORMATION where ID = ?",id):
-        result = cursor.fetchone()
-        password = result[0]
-        temp = account[0]
-
-    if(temp != account[0]):
-        return 1
-        #ans = "ID does not exist."
+    client_account = RecieveList(conn)
+    for row in cursor.execute("select PASSWORD from INFORMATION where ID = ?", client_account[0]):
+        #row trả về hàng chứa id. vd: ('nhan','123456')
+        password = row[0] 
+        temp = "checked"
+    #conx.close() 
+    if(temp != "checked"):
+        #return 1
+        ans = "ID does not exist."
     else:       
-        if(accoun[1]==passord):
-            return 2
-            #ans = "Login successfully!"
+        if(client_account[1]==password):
+            #return 2
+            ans = "Login successfully!"
         else:
-            return 3
-            #ans = "Wrong password."
-    #conn.send(ans.encode(FORMAT
-    conx.close()    
-    
-#Hàm đăng ký
-def Signup(id,pw,pwa):
-    conx = pyodbc.connect(
-    "Driver={ODBC Driver 17 for SQL Server};"
-    "Server=DESKTOP-S8G0HJG\SQLEXPRESS;"
-    "Database=ACCOUNT;"
-    "Trusted_Connection=yes;")
-    temp = ""
-    account = RecieveList(conn)
-    cursor = conx.cursor()
-    for row in cursor.execute("select * from INFORMATION where ID = ?",id):
-        result = cursor.fetchone() 
-        temp = account[0]
-    if(temp == account[0]):
-        return 1
-        #ans = "ID already exists."
-    else:       
-        if(pw==pwa):
-            return 2
-            #ans = "Successfully!"
-        else:
-            return 3
-            #ans = "Confirm password do not match."
-    #conn.send(ans.encode(FORMAT))
-    conx.close()
+            #return 3
+            ans = "Wrong password."
+    conn.send(ans.encode(FORMAT))
+    print(ans)
 
 #Hàm thêm id và pw vào database
 def Insert_ID_PW(id,pw):
-    conn = pyodbc.connect(
+    cursor = conx.cursor()
+    cursor.execute("insert INFORMATION values (?,?)", id, pw)
+    conx.commit() 
+
+#Hàm đăng ký
+def Signup(conn):
+    temp = ""
+    client_account = RecieveList(conn)
+    for row in cursor.execute("select * from INFORMATION where ID = ?",client_account[0]):
+        temp = "checked"
+    if(temp == "checked"):
+        #return 1
+        ans = "ID already exists."
+    else:
+        if(client_account[1]==client_account[2]):
+            #return 2
+            ans = "Successfully!"
+            Insert_ID_PW(client_account[0],client_account[1])
+        else:
+            #return 3
+            ans = "Confirm password do not match."
+    conn.send(ans.encode(FORMAT))
+    conn.close()
+
+def HandleClient(conn,address):
+    print("Connected to ", address)
+    print()
+    msg = conn.recv(1024).decode(FORMAT)
+    temp = " "
+    conn.send(temp.encode(FORMAT))
+    if msg == "Y":
+        AccountCheck(conn)
+    elif msg == "N":
+        Signup(conn)
+    
+    msg = conn.recv(1024).decode(FORMAT)
+    print(msg)
+    return msg
+    #conn.close()
+
+###################################### MAIN ##########################################
+conx = pyodbc.connect(
     "Driver={ODBC Driver 17 for SQL Server};"
     "Server=DESKTOP-S8G0HJG\SQLEXPRESS;"
     "Database=ACCOUNT;"
     "Trusted_Connection=yes;")
-    cursor = conn.cursor()
-    cursor.execute("insert INFORMATION values (?,?)", id, pw)
-    conn.commit()   
-    conn.close()
-
-###################################### MAIN ##########################################
+cursor = conx.cursor()
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #SOCK_STREAM: giao thức TCP
 server.bind((SERVER, PORT))
 
@@ -91,97 +94,17 @@ server.listen()
 print("Server is waiting...")
 print()
 
-try:
-    conn, address = server.accept()
-    print("Connected to ", address)
-    print()
-    #nickname=client.recv(1024).decode(FORMAT)
-    #print("Nickname of the client: ", nickname)
-    conn.send("Connected to the server! Do you have an account? [Y/N]".encode(FORMAT))
-    
-    while(True):
-        message=conn.recv(1024).decode(FORMAT)
-        if(message=="Y"):
-            #Gửi yêu cầu nhập ID và nhận ID client trả về
-            Answer = "Enter ID:"
-            conn.send(Answer.encode(FORMAT))
-            id = conn.recv(1024).decode(FORMAT)
-            print(id)
-            
-            #Gửi yêu cầu client nhập pw và nhận pw trả về 
-            Answer = "Enter Password: "
-            conn.send(Answer.encode(FORMAT))
-            pw = conn.recv(1024).decode(FORMAT)
-            print(pw)
-            
-            #Kiểm tra id, pw và gửi kết quả cho client.
-            temp = AccountCheck(id,pw)
-            print(temp)
-
-        elif(message=="N"):
-            Answer = "Create ID:"
-            conn.send(Answer.encode(FORMAT))
-            id = conn.recv(1024).decode(FORMAT)
-            print(id)
-            
-            #Gửi yêu cầu client nhập pw và nhận pw trả về 
-            Answer = "Enter Password: "
-            conn.send(Answer.encode(FORMAT))
-            pw = conn.recv(1024).decode(FORMAT)
-            print(pw)
-
-            #gửi yêu cầu nhập lại mật khẩu
-            Answer = "Enter Password again."
-            conn.send(Answer.encode(FORMAT))
-            pw = conn.recv(1024).decode(FORMAT)
-            print(pwa)
-
-            #Kiểm tra id, pw và gửi kết quả cho client.
-            temp =  Signup(id,pw,pwa) 
-            print(temp)
-            
-
-        elif(message=="quit"):   #Nếu đặt break dưới 2 lệnh sau thì server sẽ phải gửi thêm 1 tin cho client 
-            break              #dù client đã yêu cầu ngắt kết nối và không có nhu cầu nhận thêm tin.
-            
-        
-        
-except:
-   print("Client ",address, "is disconnected.") #Nếu client thoát đột ngột => chạy dòng này => server không bị treo.
-
-
+msg = ""
+while(msg!="end"):
+    try:
+        conn, address = server.accept()
+        msg = HandleClient(conn,address)                    
+    except:
+        print("Client ",address, "is disconnected.") #Nếu client thoát đột ngột => chạy dòng này => server không bị treo.
+        msg = "end"
 conn.close()
 print("Connection with ", address, " ended")
 server.close()
-#conn = pyodbc.connect(
-#   "Driver={ODBC Driver 17 for SQL Server};"
-#    "Server=DESKTOP-S8G0HJG\SQLEXPRESS;"
-#    "Database=ACCOUNT;"
-#    "Trusted_Connection=yes;")
-
-#cursor = conn.cursor()
-
-#for row in cursor.execute("select * from INFORMATION where ID = 'phanthiennhan'"):
-#   print(row.ID)
-#   print(row[0])
-#   print(row)
-
-#cursor.execute("select * from INFORMATION")
-#data = cursor.fetchall()
-#print(data)
-#print(data[1][0])
-#print(data[1][1])
-
-#cursor.execute("insert INFORMATION values ('test','321')") #thêm id và pass vào database
-#conn.commit()  #lưu lại thông tin được insert
-
-#id = 'abc'
-#pw = '111'
-#cursor.execute("insert INFORMATION values (?,?)", id, pw)
-#conn.commit()   
-#conn.close()
-
-#server.close()
 
 
 
