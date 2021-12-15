113aac3a3af6273ad22d7811ae7c19f479990f71
Phan Thiện Nhân <84320584+NHANSMALL999@users.noreply.github.com>
2021-12-15 18:44:39 +0700

    Update gui_client.py

--- gui_client.py
+++ gui_client.py
@@ -1,7 +1,16 @@
+#Hàm xử lý
+import socket
+import threading
+SERVER=socket.gethostbyname(socket.gethostname())
+PORT = 8000
+FORMAT = "utf_16"
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #SOCK_STREAM: giao thức TCP
+        
+
+#Hàm cho GUI
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk 
-import pyodbc
 
 
 HEADER_FONT = ("Open Sans", 16,"bold")
@@ -14,76 +23,6 @@ ERROR = -1
 WARNING = 0
 INFO = 1
 
-
-################# HÀM XỬ LÝ ###############
-#Hàm gửi danh sách
-def SendList(client, list):
-    for data in list:
-        client.send(data.encode(FORMAT))
-        client.recv(1024)
-    msg = "end"
-    client.send(msg.encode(FORMAT))
-
-#Hàm đăng nhập
-def Signin(client,id, pw):
-    list = []
-    #id = input("ID: ")
-    list.append(id)
-    #pw = input("Password: ")
-    list.append(pw)
-    SendList(client,list)
-    
-#Hàm đăng ký
-def Signup(client):
-    list = []
-    id = input("ID: ")
-    list.append(id)
-    pw = input("Password: ")
-    list.append(pw)
-    pwa = input("Input password again: ")
-    list.append(pwa)
-    SendList(client,list)
-
-
-#Hàm nhận kết quả đăng nhập
-def ResultSignin():
-    result = client.recv(1024).decode(FORMAT)
-    if(result=="Login successfully!"):
-        return 0
-        #print("ID was wrong")
-    elif(result=="ID does not exist."):
-        return 1
-        #print("Successfully!")
-    elif(result=="Wrong password."):
-        return 2
-        #print("Password was wrong! Enter again!")
-    else:
-        return -1
-        #print("Connection was corrupted!")
-        
-#Hàm nhận kết quả đăng ký
-def ResultSignup(client):
-    result = client.recv(1024).decode(FORMAT)
-    if(result=="ID already exists."):
-        #return 1
-        print("ID already exists. Please choose other ID")
-    elif(result == "Successfully!"):
-        #return 2
-        print("Sign up successfully! Sign in now!")
-    elif(result=="Confirm password do not match."):
-        #return 3
-        print("Confirm password do not match. Enter again!")
-
-###################
-def click_login(controller, client, id, pw):
-    Signin(client, id, pw)
-    check = ResultSignin(client)
-    if check==0:
-        controller.showFrame(HomePage)
-    elif check==1 || check==2:
-        notification(ERROR, "Wrong password or username!!!\nPlease try again.")
-    else:
-        notification(ERROR, "Connection was corrupted!!!")
 
 ##################
 def notification(type, message):
@@ -151,7 +90,8 @@ class StartPage(tk.Frame):
         self.entry_user = tk.Entry(frame_1,width=20,bg='#EBEBF2', font=REGULAR_FONT)
         self.entry_pswd = tk.Entry(frame_1,width=20,bg='#EBEBF2', font=REGULAR_FONT)
 
-        button_log = tk.Button(frame_1,text="LOG IN",font=BUTTON_FONT, bg="#6B8DF2",fg='#EBEBF2',command=lambda:click_login(controller, self.entry_user.get(), self.entry_pswd.get())) 
+        
+        button_log = tk.Button(frame_1,text="LOG IN",font=BUTTON_FONT, bg="#6B8DF2",fg='#EBEBF2',command=lambda:click_login(controller, client, str(self.entry_user.get()), str(self.entry_pswd.get()))) 
         button_log.configure(width=10)
         button_sign = tk.Button(frame_1,text="SIGN UP",font=BUTTON_FONT,bg="#6B8DF2",fg='floral white', command=lambda:controller.showFrame(SignUpPage)) 
         button_sign.configure(width=10)
@@ -205,10 +145,14 @@ class SignUpPage(tk.Frame):
         self.entry_user = tk.Entry(frame_1,width=20,bg='#EBEBF2', font=REGULAR_FONT)
         self.entry_pswd = tk.Entry(frame_1,width=20,bg='#EBEBF2', font=REGULAR_FONT)
 
-        button_log = tk.Button(frame_1,text="SIGN UP",font=BUTTON_FONT, bg="#6B8DF2",fg='#EBEBF2',command=lambda:controller.showFrame(HomePage)) 
-        button_log.configure(width=10)
-        button_sign = tk.Button(frame_1,text="LOG IN",font=BUTTON_FONT,bg="#6B8DF2",fg='floral white', command=lambda:notification(ERROR, "Wrong password or username!!!\nPlease try again.")) 
-        button_sign.configure(width=10)
+        #id = self.entry_user.get()
+        #pw = self.entry_pswd.get()
+
+        button_signup = tk.Button(frame_1,text="SIGN UP",font=BUTTON_FONT, bg="#6B8DF2",fg='#EBEBF2',command=lambda:click_signup(controller, client, str(self.entry_user.get()), str(self.entry_pswd.get()))) 
+
+        button_signup.configure(width=10)
+        button_returnlog = tk.Button(frame_1,text="LOG IN",font=BUTTON_FONT,bg="#6B8DF2",fg='floral white', command=lambda:controller.showFrame(StartPage)) 
+        button_returnlog.configure(width=10)
 
         #########################################################################
         canvas = tk.Canvas(self, width=230, height=300, bg='#6B8DF2', bd=0)
@@ -226,16 +170,14 @@ class SignUpPage(tk.Frame):
         self.entry_pswd.grid(row=5,column=3, sticky='nsew')
 
         self.label_blank_2.grid(row=6,column=3)
-        button_log.grid(row=7,column=3)
+        button_signup.grid(row=7,column=3)
 
         self.label_blank_3.grid(row=8,column=3)
-        button_sign.grid(row=9,column=3)
+        button_returnlog.grid(row=9,column=3)
 
         canvas.place(x=0,y=0)
         frame_1.place(x=320, y=30)
         ####################################################################
-
-
 
 
 class HomePage(tk.Frame):
@@ -243,9 +185,60 @@ class HomePage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.configure(bg="bisque2")
         
-        
+###################
+def click_login(controller, client, id, pw):
+    try:
+        #Gui lua chon
+        client.send("logIn".encode(FORMAT))
+        client.recv(1024)
+        #Gui username
+        client.send(id.encode(FORMAT))
+        client.recv(1024)
+        #Gui password
+        client.send(pw.encode(FORMAT))
+        client.recv(1024)
+    
+        check = int(client.recv(1024).decode(FORMAT))
+        if check==0:
+            controller.showFrame(HomePage)
+        elif check==1:
+            notification(ERROR, "Wrong username!!!\nPlease try again.")
+        elif check==2:
+            notification(ERROR, "Wrong password!!!\nPlease try again.")
+        else:
+            notification(ERROR, "Connection was corrupted!!!")
+    except:
+        notification(ERROR, "Connection was corrupted!!!")
+           
+def click_signup(controller, client, id, pw):
+    try:
+        #Gui lua chon
+        client.send("signUp".encode(FORMAT))
+        client.recv(1024)
+        #Gui username
+        client.send(id.encode(FORMAT))
+        client.recv(1024)
+        #Gui password
+        client.send(pw.encode(FORMAT))
+        client.recv(1024)
 
+        check=int(client.recv(1024).decode(FORMAT))
+        if check==0:
+            controller.showFrame(HomePage)
+        elif check==1:
+            notification(ERROR, "User has already existed!!!\nPlease choose other user.")
 
+        else:
+             notification(ERROR, "Connection was corrupted!!!")
+    except: 
+        notification(ERROR, "Connection was corrupted!!!")
+try:
+    client.connect((SERVER, PORT))
+    app = VndEx_App()
+    app.mainloop()
 
-app = VndEx_App()
-app.mainloop()
+            
+except: 
+    print("CAN NOT CONNECT TO SERVER") #Nếu server chưa mở => không kết nối được => báo lỗi 
+                                        #=> vẫn chạy dòng client.close => không bị treo
+client.close()
